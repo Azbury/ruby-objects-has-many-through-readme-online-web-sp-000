a906b432d664b56d49c4d389bc2c1ae5303d35fb
Austin Asbury <acasbury25@aol.com>
2019-09-01 12:41:54 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -23,9 +23,11 @@ class Waiter
     end
   end
 
-  def waiters
-    meals.map do |meal|
-      meal.waiter
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
     end
+    best_tipped_meal.customer
   end
+
 end
